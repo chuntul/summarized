c7e43d33d1b8c476269a1eb5faeec36ad6c7f990
Chantal <chantal.ho98@hotmail.com>
2020-01-12 00:43:23 -0500

    weird changes that werent shown on github even though they shouldve been

---
+++ src/TodoList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './TodoContainer.css'
+
+const TodoList = props => (
+  <ul>
+    {
+      props.items.map((item, index) => <li key={index}>{item}</li>)
+    }
+  </ul>
+);
+
+export default TodoList;
